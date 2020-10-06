2b9b2d142385651291d0ab258401de3a0bf7782b
MeLIoDs21 <54976266+MeLIoDs21@users.noreply.github.com>
2020-10-06 11:40:58 +0200

    Add files via upload

---
+++ Template/Template/BaseObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Template
+{
+    class BaseObject
+    {
+        Texture2D _Object;
+        Vector2 _Pos;
+        Rectangle _HitBox;
+        public BaseObject(Vector2 pos, Rectangle hitBox)
+        {
+
+            _Pos = pos;
+            _HitBox = hitBox;
+
+        }
+
+        public void LoadContent(ContentManager content, string fileName)
+        {
+            _Object = content.Load<Texture2D>(fileName);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(_Object, _HitBox, Color.Black);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Template/Template/Test.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Test 
+    {
+        Texture2D _Object;
+        Vector2 _Pos;
+        Rectangle _HitBox;
+        public Test(Vector2 pos)
+        {
+
+
+
+            _Pos = pos;
+            _HitBox = new Rectangle(0, 0, 100, 100);
+            
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            
+            _Object = Content.Load<Texture2D>("shuttle");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(_Object, _HitBox, Color.Black);
+            spriteBatch.End();
+        }
+    }
+}
